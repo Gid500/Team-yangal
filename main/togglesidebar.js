cbe5c84ec3833279689ebd14e221c06d361e9236
John Doe <ssqod1202@gmail.com>
2025-04-21 15:08:19 +0900

    T

--- main/togglesidebar.js
+++ main/togglesidebar.js
@@ -15,28 +15,25 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-
-
-
 function toggleSidebar() {
-    const sidebar = document.getElementById("sidebar");
-    const overlay = document.getElementById("overlay");
-    const menuBtn = document.getElementById("menuBtn");
-    const isOpen = sidebar.classList.contains("active");
-  
-    if (isOpen) {
-    sidebar.classList.remove("active");
-    overlay.classList.remove("active");
-    menuBtn.style.display = "block";
-    } else {
-    sidebar.classList.add("active");
-    overlay.classList.add("active");
-    menuBtn.style.display = "none";
-    }
+  const sidebar = document.getElementById("sidebar");
+  const overlay = document.getElementById("overlay");
+  const menuBtn = document.getElementById("menuBtn");
+  const isOpen = sidebar.classList.contains("active");
+
+  if (isOpen) {
+  sidebar.classList.remove("active");
+  overlay.classList.remove("active");
+  menuBtn.style.display = "block";
+  } else {
+  sidebar.classList.add("active");
+  overlay.classList.add("active");
+  menuBtn.style.display = "none";
+  }
 }
   
 function closeSidebar() {
-    document.getElementById("sidebar").classList.remove("active");
-    document.getElementById("overlay").classList.remove("active");
-    document.getElementById("menuBtn").style.display = "block";
+  document.getElementById("sidebar").classList.remove("active");
+  document.getElementById("overlay").classList.remove("active");
+  document.getElementById("menuBtn").style.display = "block";
 }
